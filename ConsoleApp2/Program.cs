2943696aeeb767b58ab8f597776ebf8e34f91ca4
James Titus <jamesttitusjr@outlook.com>
2023-01-28 08:37:42 -0500

    Add project files.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,14 @@
+﻿/*
+using System;
+
+Console.WriteLine("Hello, \nWorld!"); \n = New line.
+
+System.Console.WriteLine();
+
+Console.BackgroundColor = ConsoleColor.Yellow;
+Console.ForegroundColor = Co
+Console.ResetColor();
+string response = Console.ReadLine();
+Console.WriteLine("Hello " + response.Substring(0, 3));
+*/
+
